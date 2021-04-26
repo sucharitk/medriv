d3df7a13a31a7c14c6da9b39856ee2ddb3e63643
sucharitk <sucharit.katyal@gmail.com>
2021-04-26 18:19:56 +0530

    Update medriv_riv_durations.m

--- processing/medriv_riv_durations.m
+++ processing/medriv_riv_durations.m
@@ -1,20 +1,13 @@
 function [exp_medriv, indivdurs, indivruns] = medriv_riv_durations(exp_medriv)
 
+% calculate the rivalry durations for each subject
+
 cd(exp_medriv.session_dir)
 
 nsubj = exp_medriv.nsubj;
 
-if nargout>1
-    indiv_durs = [];
-    indivdur_perc = [];
-    indivdur_group = [];
-    indivdur_block = [];
-    indivdur_runnum = [];
-    indivdur_subj = [];
-    indivdur_valid = [];
-    indivdur_eyedom = [];
-end
 rem_last = true; % remove the last duration because it is incomplete
+
 for ns = 1:nsubj
     
     subj_data = exp_medriv.data(ns);
@@ -22,7 +15,7 @@ for ns = 1:nsubj
     if subj_data.subj_valid_riv
         cd(fullfile(exp_medriv.session_dir, subj_data.dir_name))
         
-        % evaluate rivalry durations
+        % load the rivalry behaviour file
         fname = sprintf('BR_Rivalry_%g_%s', subj_data.subj_code,...
             subj_data.session_date);
         load(fullfile('Behavior', fname))
@@ -30,11 +23,15 @@ for ns = 1:nsubj
         if isempty(subj_data.riv_runs)
             riv_runs = {1:5, 6:10, 11:15};
         else
+            % for subjects that had blocks within fewer than 5 runs
             riv_runs = subj_data.riv_runs;
         end
+        
         nblocks = numel(riv_runs);
         numrivrun = 0;
         for nb = 1:nblocks
+        
+            %%% call function to calculate the individual rivalry duraitons for each block as a whole
             [dur_all, trans_times] = plot_rivalry_responses(results, riv_runs{nb},...
                 [], 0, [], [], [], rem_last);
             
@@ -67,6 +64,8 @@ for ns = 1:nsubj
             
             exp_medriv.data(ns).riv_durations(nb);
             
+            
+            %%% calculate rivalry runs for each individual run within a block
             nrr = numel(riv_runs{nb});
             for numr = 1:nrr
                 dur_all = plot_rivalry_responses(results, riv_runs{nb}(numr),...
@@ -81,91 +80,14 @@ for ns = 1:nsubj
                     [median(dur_all{1}), median(dur_all{2}), median(dur_all{3})];
                 exp_medriv.data(ns).riv_run_durs(numrivrun).median_all = ...
                     median([dur_all{1} dur_all{2}]);
-                
-                if nargout==2
-                    ndurs = [numel(dur_all{1}) numel(dur_all{2}) ...
-                        numel(dur_all{3})];
-                    indiv_durs = [indiv_durs dur_all{1} dur_all{2} ...
-                        dur_all{3}];
-                    indivdur_perc = [indivdur_perc 1*ones(1, ndurs(1)), ...
-                        2*ones(1, ndurs(2)), 3*ones(1, ndurs(3))];
-                    indivdur_group = [indivdur_group ...
-                        subj_data.group*ones(1, sum(ndurs))];
-                    indivdur_block = [indivdur_block ...
-                        nb*ones(1, sum(ndurs))];
-                    indivdur_runnum = [indivdur_runnum  ...
-                        numr*ones(1, sum(ndurs))];
-                    indivdur_subj = [indivdur_subj  ...
-                        subj_data.subj_code*ones(1, sum(ndurs))];
-                    indivdur_valid = [indivdur_valid ...
-                        subj_data.subj_valid_riv*ones(1, sum(ndurs))];
-                    indivdur_eyedom = [indivdur_eyedom...
-                        exp_medriv.data(ns).riv_durations(nb).eye_dominance*...
-                        ones(1, sum(ndurs))];
-                end
-                if nargout==3
-                    ndurs = [numel(dur_all{1}) numel(dur_all{2}) ...
-                        numel(dur_all{3})];
-                    indiv_durs = [indiv_durs mean([dur_all{1} dur_all{2}])];
-                    indivdur_perc = [indivdur_perc 1];
-                    indivdur_group = [indivdur_group ...
-                        subj_data.group];
-                    indivdur_block = [indivdur_block ...
-                        nb];
-                    indivdur_runnum = [indivdur_runnum  ...
-                        numr];
-                    indivdur_subj = [indivdur_subj  ...
-                        subj_data.subj_code];
-                    indivdur_valid = [indivdur_valid ...
-                        subj_data.subj_valid_riv];
-                    indivdur_eyedom = [indivdur_eyedom...
-                        exp_medriv.data(ns).riv_durations(nb).eye_dominance];
-                    if ndurs(3)>0
-                        indiv_durs = [indiv_durs mean([dur_all{3}])];
-                        indivdur_perc = [indivdur_perc 2];
-                        indivdur_group = [indivdur_group ...
-                            subj_data.group];
-                        indivdur_block = [indivdur_block ...
-                            nb];
-                        indivdur_runnum = [indivdur_runnum  ...
-                            numr];
-                        indivdur_subj = [indivdur_subj  ...
-                            subj_data.subj_code];
-                        indivdur_valid = [indivdur_valid ...
-                            subj_data.subj_valid_riv];
-                        indivdur_eyedom = [indivdur_eyedom...
-                            exp_medriv.data(ns).riv_durations(nb).eye_dominance];
-                    end
-                end
             end
+            
         end
     end
 end
 
 fprintf('durations saved!\n')
 
-if nargout==2
-    indivdurs.durs = indiv_durs;
-    indivdurs.perc = indivdur_perc;
-    indivdurs.group = indivdur_group;
-    indivdurs.block = indivdur_block;
-    indivdurs.runnum = indivdur_runnum;
-    indivdurs.subj = indivdur_subj;
-    indivdurs.valriv = indivdur_valid;
-    indivdurs.eyedom = indivdur_eyedom;
-end
-
-if nargout==3
-    indivdurs = [];
-    indivruns.durs = indiv_durs;
-    indivruns.perc = indivdur_perc;
-    indivruns.group = indivdur_group;
-    indivruns.block = indivdur_block;
-    indivruns.runnum = indivdur_runnum;
-    indivruns.subj = indivdur_subj;
-    indivruns.valriv = indivdur_valid;
-    indivruns.eyedom = indivdur_eyedom;
-end
 cd(exp_medriv.session_dir)
 
 end
